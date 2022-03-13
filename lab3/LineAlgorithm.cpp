7aba656c42611b6c66a49ed8bfd38314db44cfb8
Jora <maki-333@yandex.ru>
2022-03-14 00:40:26 +0300

    implement Wu

--- lab3/LineAlgorithm.cpp
+++ lab3/LineAlgorithm.cpp
@@ -5,7 +5,7 @@ static inline int sign(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-static inline QColor colorBlend(QColor a, QColor b, float ratio) {
+static inline QColor colorBlend(QColor a, QColor b, double ratio) {
     return QColor(
         a.red() * ratio + b.red() * (1 - ratio),
         a.green() * ratio + b.green() * (1 - ratio),
@@ -39,10 +39,14 @@ void algorithmBrezenhamFloat(QImage &pixmap, const QLine &line, const QColor &co
 
     int stepX = sign(deltaX);
     int stepY = sign(deltaY);
+
+    deltaX = abs(deltaX);
+    deltaY = abs(deltaY);
+
     int x = line.x1();
     int y = line.y1();
 
-    if (abs(deltaX) > abs(deltaY)) {
+    if (deltaX > deltaY) {
         qreal error = qreal(deltaY) / deltaX - 0.5;
         while (x != line.x2()) {
             pixmap.setPixel(x, y, color.rgba());
@@ -78,10 +82,14 @@ void algorithmBrezenhamInt(QImage &pixmap, const QLine &line, const QColor &colo
 
     int stepX = sign(deltaX);
     int stepY = sign(deltaY);
+
+    deltaX = abs(deltaX);
+    deltaY = abs(deltaY);
+
     int x = line.x1();
     int y = line.y1();
 
-    if (abs(deltaX) > abs(deltaY)) {
+    if (deltaX > deltaY) {
         int error = 2 * deltaY - deltaX;
         while (x != line.x2()) {
             pixmap.setPixel(x, y, color.rgba());
@@ -165,4 +173,62 @@ void algorithmBrezenhamAntialias(QImage &pixmap, const QLine &line, const QColor
     }
 }
 
-void algorithmWu(QImage &pixmap, const QLine &line, const QColor &color) {}
+void algorithmWu(QImage &pixmap, const QLine &line, const QColor &color) {
+    int deltaX = line.x2() - line.x1();
+    int deltaY = line.y2() - line.y1();
+
+    int stepX = sign(deltaX);
+    int stepY = sign(deltaY);
+
+    deltaX = abs(deltaX);
+    deltaY = abs(deltaY);
+
+    int x = line.x1();
+    int y = line.y1();
+
+    qreal maxI = 1.;
+    qreal error = maxI / 2.;
+
+    if (deltaX > deltaY) {
+        qreal m = qreal(maxI) * deltaY / deltaX;
+        qreal w = maxI - m;
+
+        while (x != line.x2()) {
+            QColor clr = colorBlend(color, pixmap.pixelColor(x, y), maxI - error);
+            pixmap.setPixelColor(x, y, clr);
+            clr = colorBlend(color, pixmap.pixelColor(x, y + stepY), error);
+            pixmap.setPixelColor(x, y + stepY, clr);
+
+            x += stepX;
+
+            if (error <= w) {
+                error += m;
+            }
+            else {
+                y += stepY;
+                error -= w;
+            }
+        }
+    }
+    else {
+        qreal m = qreal(maxI) * deltaX / deltaY;
+        qreal w = maxI - m;
+
+        while (y != line.y2()) {
+            QColor clr = colorBlend(color, pixmap.pixelColor(x, y), maxI - error);
+            pixmap.setPixelColor(x, y, clr);
+            clr = colorBlend(color, pixmap.pixelColor(x + stepX, y), error);
+            pixmap.setPixelColor(x + stepX, y, clr);
+
+            y += stepY;
+
+            if (error <= w) {
+                error += m;
+            }
+            else {
+                x += stepX;
+                error -= w;
+            }
+        }
+    }
+}
